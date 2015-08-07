8308f14fde2ef93cdf9eb65bfa4156197838e37a
Thach <thachmle@aol.com>
2015-08-07 00:47:57 -0400

    changes to page title

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,6 @@ if(window.XMLHttpRequest) {
 } else {
 	rq = new  ActionXObject("Microsoft.XMLHTTP");
 }
-
 rq.open('GET','data.xml');
 rq.onreadystatechange = function() {
 	if(rq.status == 200 && rq.readyState === 4) {
@@ -16,21 +15,26 @@ rq.send();
 
 $('#mods').click(function() {
 	$('title').html('Mods');
+	$('.page-title').html('MODS');
 	$('.content-left').load('index2.html');
+
 })
 
 $('#hacks').click(function(){
 	$('title').html('Hacks');
+	$('.page-title').html('HACKS');
 	$('.content-left').load("index3.html");
 });
 
 $('#taunts').click(function() {
 	$('title').html('Taunts');
+	$('.page-title').html('TAUNTS');
 	$('.content-left').load('index4.html');
 });
 
 $('#video').click(function() {
 	$('title').html('Videos');
+	$('.page-title').html('VIDEOS');
 	$('.content-left').load('index5.html');
 })
 
